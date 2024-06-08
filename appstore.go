257522b8d8e779214789fc856686d3b4c7248a9e
Oleksandr Chaikovskyi <alex.programmr@gmail.com>
2024-06-08 12:08:37 +0200

    Delete achievement and achievement localization

--- appstore.go
+++ appstore.go
@@ -91,3 +91,29 @@ func doCreate[T any](c *Client, ctx context.Context, url string, data any) (*T, 
 
 	return &rp.Data, nil
 }
+
+func doDelete(c *Client, ctx context.Context, url string) error {
+	req, err := http.NewRequestWithContext(ctx, http.MethodDelete, url, http.NoBody)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+
+	token, err := c.tokenSource.Token()
+	if err != nil {
+		return fmt.Errorf("failed to get token: %w", err)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	return nil
+}
